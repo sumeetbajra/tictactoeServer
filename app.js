a5ae8f004e80006858c21bc017378173c4ebbaa8
Sumit <sumeetbajra@gmail.com>
2016-11-11 02:00:19 +0545

    exit room

--- app.js
+++ app.js
@@ -7,21 +7,37 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+	var rooms = {};
 	var room = Date.now();
-	socket.join(room)
+	socket.join(room);
 
 	socket.on('createRoom', function() {
 		room = Date.now();
 		socket.join(room)
 		io.in(room).emit('roomId', room);
+		rooms[room] = socket.id;
 	})
 
 	socket.on('joinRoom', function(room){
-		socket.join(room)
+		socket.join(room);
+		var roomUsers = io.sockets.adapter.rooms[room];
+		socket.emit('newUserCount', roomUsers['length']);
+		io.in(room).emit('userCount', roomUsers['length']);
+		rooms[room] = socket.id;
 	});	
 
+	socket.on('disconnect', function() {
+    	Object.keys(rooms).filter(function(room) {
+    		if(rooms[room] === socket.id) {
+    			var roomUsers = io.sockets.adapter.rooms[room];
+    			if(roomUsers) {
+    				io.in(room).emit('userCount', roomUsers['length']);
+    			}
+    		}
+    	})
+	})
+
 	socket.on('restartGame', function(room) {
-		console.log(room);
 		socket.broadcast.to(room).emit('restartGame');
 	})
 
